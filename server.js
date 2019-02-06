fb5363319e49c597384cbd62d6bfe7d298d374d6
Owen Canales <oc9835@mcla.edu>
2019-02-06 16:37:25 +0000

    (upload of Assignment #1)

--- server.js
+++ server.js
@@ -16,19 +16,70 @@ function requestHandler(req, res)
     
     res.writeHead(200, {'Content-Type': 'text/html'});
     
-    if (query['cmd'] == 'add')
+    //Part 1:
+    if (query['cmd'] == 'repeat')
     {
       console.log("Handling a request");
       console.log(query);
-      var sum = 0;
-      for (var i in query['num'])
+      //var word = query['word'];
+      //var i;
+      for (var i = 0; i < query['word'].length; i++)
       {
-        sum = sum + parseInt(query['num'][i]);
+            res.write('<pre>'+query['word']+'</pre>');
       }
-      
-      res.write('<p>'+sum+'</p>');
       res.end('');
     }
+    
+    //part 2:
+    if (query['cmd'] == 'dotted')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var word1 = query['word1'];
+      res.write('<pre>'+ query['word1']);
+      var word2 = query['word2'];
+      var total = (query['word1'].length+query['word2'].length);
+      for (i = 30; i > total; i--)
+      {
+        res.write('.');
+      }
+      res.write(query['word2']+'</pre>');
+      
+      res.end('');
+    }
+    
+    //part 3:
+    if (query['cmd'] == 'stats')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      //var num = query['grades'].length;
+      var max = 0;
+      var min = 1000;
+      var avg = 0;
+      for (var i in query['grades'])
+      {
+        //max
+        if (max < query['grades'][i])
+        {
+          max = query['grades'][i];
+        }
+        //min
+        if (min > query['grades'][i])
+        {
+          min = query['grades'][i];
+        }
+        //average
+        avg = avg + parseInt(query['grades'][i]);
+        
+      }
+      avg = avg/query['grades'].length;
+      res.write('<pre>Avg: '+avg+' | Min: '+min+' | Max: '+max+'</pre>');
+      
+      res.end('');
+    }
+    
+    
     else
     {
       res.end('');
